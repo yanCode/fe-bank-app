d8caeb1cf712bb75b084b34fa26609fda07bcf45
ShengYan, Zhang <yan.code@gmail.com>
2023-02-07 08:07:29 +0800

    chore: intial add

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Stats from "./Stats.jsx";
+import Business from "./Business.jsx";
+import Billing from "./Billing.jsx";
+
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing
+};
